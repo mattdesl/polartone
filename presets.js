21c449e3efd4a5b0b668744d104d774db62f566f
mattdesl <dave.des@gmail.com>
2016-04-10 13:57:24 -0700

    fix presets

--- presets.js
+++ presets.js
@@ -1,22 +1,22 @@
 module.exports = [
-  {
-    capacity: 1000,
-    distance: 0.2,
-    alpha: 0.05,
-    seek: 0,
-    extent: 1.0,
-    position: [0, -3.5, 0],
-    url: 'https://soundcloud.com/max-richter/5-sarajevo'
-  },
-  {
-    capacity: 1000,
-    seek: 0,
-    distance: 0.2,
-    alpha: 0.35,
-    extent: 0.5,
-    position: [0, -3.5, 0],
-    url: 'https://soundcloud.com/max-richter/5-sarajevo'
-  },
+  // {
+  //   capacity: 1000,
+  //   distance: 0.2,
+  //   alpha: 0.05,
+  //   seek: 0,
+  //   extent: 1.0,
+  //   position: [0, -3.5, 0],
+  //   url: 'https://soundcloud.com/max-richter/5-sarajevo'
+  // },
+  // {
+  //   capacity: 1000,
+  //   seek: 0,
+  //   distance: 0.2,
+  //   alpha: 0.35,
+  //   extent: 0.5,
+  //   position: [0, -3.5, 0],
+  //   url: 'https://soundcloud.com/max-richter/5-sarajevo'
+  // },
   {
     capacity: 900,
     distance: 0.35,
@@ -28,20 +28,28 @@ module.exports = [
   },
   {
     capacity: 1000,
-    distance: 0.2,
-    alpha: 0.15,
+    distance: 0.1,
+    alpha: 0.2,
     extent: 0.25,
     position: [0, 3.5, 0],
-    url: 'https://soundcloud.com/futureclassic/hayden-james-something-about-you-2'
+    url: 'https://soundcloud.com/kartell/sg-lewis-no-less-kartell-remix-1'
   },
   {
-    capacity: 500,
-    distance: 0.01,
-    extent: 0.2,
-    alpha: 0.7,
+    capacity: 400,
+    distance: 0.2,
+    alpha: 0.25,
+    extent: 0.50,
     position: [0, 3.5, 0],
-    url: 'https://soundcloud.com/futureclassic/hayden-james-something-about-you-2'
-  }
+    url: 'https://soundcloud.com/maddecent/unlike-pluto-kickraux-palace-1'
+  },
+  // {
+  //   capacity: 500,
+  //   distance: 0.01,
+  //   extent: 0.2,
+  //   alpha: 0.7,
+  //   position: [0, 3.5, 0],
+  //   url: 'https://soundcloud.com/futureclassic/hayden-james-something-about-you-2'
+  // }
   // {
   //   capacity: 1000,
   //   distance: 0.09,
